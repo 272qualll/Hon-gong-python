bd9d26179450a3dd55e7fcea7ae439581c433c93
272qualll <141555822+272qualll@users.noreply.github.com>
2024-04-07 22:37:37 +0900

    Update chapter03-02.py

--- chapter03-02.py
+++ chapter03-02.py
@@ -12,12 +12,12 @@ if num % 3 == 0:
 else:
     print(f"{num} is not number divided by 3")
     
-if num % 2 == 0:
-    print(f"{num} is number divided by 3")
+if num % 5 == 0:
+    print(f"{num} is number divided by 5")
 else:
-    print(f"{num} is not number divided by 3")
+    print(f"{num} is not number divided by 5")
     
-if num % 2 == 0:
-    print(f"{num} is number divided by 3")
+if num % 7 == 0:
+    print(f"{num} is number divided by 7")
 else:
-    print(f"{num} is not number divided by 3")
+    print(f"{num} is not number divided by 7")
